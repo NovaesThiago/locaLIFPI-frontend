0528ee068266449e3837fd52cdd429f6ed687483
Fernanda Araújo <fernandacomun@gmail.com>
2025-07-25 22:05:05 -0300

    v

--- script/map.vue.js
+++ script/map.vue.js
@@ -644,6 +644,7 @@ createApp({
         {
           nome: "Bloco S",
           salas: [
+            //sala mambee extensao antiga
             {
               id: 64,
               nome: "Sala I6",  //biblioteca
